02f976e3452165486d652b81e17b05a9ec4abfb9
Zeta <futahei1108@icloud.com>
2018-04-26 16:53:33 +0900

    learn ValType in solidity

--- solidity/SingleNumRegister.sol
+++ solidity/SingleNumRegister.sol
@@ -1,10 +1,31 @@
 pragma solidity ^0.4.0;
 contract SingleNumRegister {
     uint storedData;
-    function set(uint x) public {
+    function set(uint x) public{
         storedData = x;
     }
-    function get() public constant returns (uint retVal) {
+
+    function get() public constant returns (uint retVal){
         return storedData;
+    }
+}
+
+contract ValTypeTest {
+    function valType() public constant returns (uint retVal){
+        uint a;
+        a = 10;
+        uint b = a;
+        b = 20;
+        return a; // result shows 10
+    }
+
+    function refType() public constant returns (uint[2] retVal){
+        uint[2] x;
+        x[0] = 100;
+        x[1] = 200;
+        uint[2] y = x;
+        y[0] = 500;
+        y[1] = 600;
+        return x; // result shows [500,600]
     }
 }
